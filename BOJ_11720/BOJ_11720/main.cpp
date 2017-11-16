4dbeedec472b7b79dbde065205b6450d65439398
Esk <skyvvv624@naver.com>
2017-11-16 23:32:45 +0900

    add

--- BOJ_11720/BOJ_11720/main.cpp
+++ BOJ_11720/BOJ_11720/main.cpp
@@ -3,22 +3,19 @@ https://www.acmicpc.net/problem/11720
 */
 
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main(void)
 {
-	char ch[100] = { 0, };
-	scanf("%s", ch);
+	int size;
+	scanf("%d", &size);
 
-	int length = strlen(ch);
-
-	for (int i = 0; i < length; i++)
-	{
-		if (i != 0 && i % 10 == 0)
-			printf("\n");
-		printf("%c", ch[i]);
-	}
-
+	char* arr = new char[size + 1];
+	int total = 0;
+	scanf("%s", arr);
+	for (int i = 0; i < size; i++)
+		total += arr[i] - 48;
+	printf("%d", total);
+	delete[] arr;
 	return 0;
 }
---
+++ BOJ_11721/BOJ_11721/main.cpp
@@ -0,0 +1,24 @@
+/*
+https://www.acmicpc.net/problem/11721
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(void)
+{
+	char ch[100] = { 0, };
+	scanf("%s", ch);
+
+	int length = strlen(ch);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (i != 0 && i % 10 == 0)
+			printf("\n");
+		printf("%c", ch[i]);
+	}
+
+	return 0;
+}
