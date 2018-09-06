85e0155a81271b14635360c2d87d02d0e2f5e258
Esk <skyvvv624@naver.com>
2018-09-06 20:33:33 +0900

    add

--- BOJ_2156/BOJ_2156/main.cpp
+++ BOJ_2156/BOJ_2156/main.cpp
@@ -7,7 +7,7 @@ void solution(int* arr, int length)
 {
 	int* dp = new int[length];
 	dp[0] = arr[0];
-	dp[1] = max(arr[1], arr[0] + arr[1]);
+	dp[1] = arr[0] + arr[1];
 	dp[2] = max(arr[0] + arr[2], arr[1] + arr[2]);
 	dp[2] = max(dp[1], dp[2]);
 
